82ef3dc7af5478d408015a2dbcab1f4668f0e602
justincatchings <justincatchings@gmail.com>
2022-08-17 22:20:05 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Tic Tac Toe
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/justincatchings/pen/GRxPNPZ](https://codepen.io/justincatchings/pen/GRxPNPZ).
+
---
+++ dist/script.js
@@ -0,0 +1,82 @@
+function Square(props) {
+  return /*#__PURE__*/(
+    React.createElement("button", { className: "square", onClick: props.onClick },
+    props.value));
+
+
+}
+
+class Board extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true };
+
+  }
+
+  renderSquare(i) {
+    return /*#__PURE__*/(
+      React.createElement(Square, {
+        value: this.state.squares[i],
+        onClick: () => this.handleClick(i) }));
+
+
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext });
+
+  }
+
+  render() {
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+
+    return /*#__PURE__*/(
+      React.createElement("div", null, /*#__PURE__*/
+      React.createElement("div", { className: "status" }, status), /*#__PURE__*/
+      React.createElement("div", { className: "board-row" },
+      this.renderSquare(0),
+      this.renderSquare(1),
+      this.renderSquare(2)), /*#__PURE__*/
+
+      React.createElement("div", { className: "board-row" },
+      this.renderSquare(3),
+      this.renderSquare(4),
+      this.renderSquare(5)), /*#__PURE__*/
+
+      React.createElement("div", { className: "board-row" },
+      this.renderSquare(6),
+      this.renderSquare(7),
+      this.renderSquare(8))));
+
+
+
+  }}
+
+
+class Game extends React.Component {
+  render() {
+    return /*#__PURE__*/(
+      React.createElement("div", { className: "game" }, /*#__PURE__*/
+      React.createElement("div", { className: "game-board" }, /*#__PURE__*/
+      React.createElement(Board, null)), /*#__PURE__*/
+
+      React.createElement("div", { className: "game-info" }, /*#__PURE__*/
+      React.createElement("div", null), /*#__PURE__*/
+      React.createElement("ol", null))));
+
+
+
+  }}
+
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render( /*#__PURE__*/React.createElement(Game, null));
